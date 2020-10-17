52e5e379dc75d1b8dd5471326892cabef4726c9f
Rasmus Halsas <raha@netlight.com>
2020-10-18 00:45:59 +0300

    Add player name generation
    Add player name generation

--- gamejam-actual/src/websocket/websocket.ts
+++ gamejam-actual/src/websocket/websocket.ts
@@ -1,5 +1,11 @@
+import { Chance } from 'chance';
+
 export const initWS = (onMessageEvent) => {
   const ws = new WebSocket('ws://31.220.7.239:8080/web-socket');
 
   ws.onmessage = onMessageEvent;
+
+  ws.onopen = () => {
+    ws.send('open-' + new Chance().name());
+  };
 };
