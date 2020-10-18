df081337d7100bc6373337545c7755d10727c2ab
Ilari Sinkkonen <ilari.sinkkonen@netlight.com>
2020-10-18 10:26:55 +0300

    Add left/right animations for killbot

--- gamejam-actual/src/scenes/animations.ts
+++ gamejam-actual/src/scenes/animations.ts
@@ -1,4 +1,4 @@
-export default (anims: Phaser.Animations.AnimationManager, spritesheet: string): void => {
+export const playerAnimation = (anims: Phaser.Animations.AnimationManager, spritesheet: string): void => {
   anims.create({
     key: `left-${spritesheet}`,
     frames: anims.generateFrameNumbers(spritesheet, { start: 0, end: 3 }),
@@ -17,5 +17,19 @@ export default (anims: Phaser.Animations.AnimationManager, spritesheet: string):
     frames: anims.generateFrameNumbers(spritesheet, { start: 5, end: 8 }),
     frameRate: 10,
     repeat: -1,
+  });
+};
+
+export const botAnimation = (anims: Phaser.Animations.AnimationManager): void => {
+  anims.create({
+    key: `bot-right`,
+    frames: [{ key: 'killbot', frame: 1 }],
+    frameRate: 10,
+  });
+
+  anims.create({
+    key: `bot-left`,
+    frames: [{ key: 'killbot', frame: 2 }],
+    frameRate: 10,
   });
 };
